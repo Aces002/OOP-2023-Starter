5a52d078dd0673f17d0e6ead26385658e88d15bd
C21459946 Grace Ojo <c21459946@mytudublin.ie>
2023-04-26 18:52:48 +0100

    labTest++++

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -8,17 +8,11 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
-	
 
 	public void settings() {
 		size(1000, 1000);
 		//fullScreen(SPAN);
 	}
-
-    String[] sonnet;
-	private String line;
-
-	null = 14;
 
     public String[] writeSonnet()
     {
